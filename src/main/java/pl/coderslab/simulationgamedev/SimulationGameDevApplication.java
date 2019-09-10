6ece9693b54d7dc9a1fd4c7c646398caa6cd85d8
Paweł <p.skarzynski81@gmail.com>
2019-09-10 11:00:51 +0200

    1st

---
+++ src/main/java/pl/coderslab/simulationgamedev/SimulationGameDevApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.simulationgamedev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulationGameDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimulationGameDevApplication.class, args);
+    }
+
+}
